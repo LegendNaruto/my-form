7fba20d174bf9c03c1168a6b4038689461c85d37
Jassem Baccari <jassembaccari0000@gmail.com>
2022-02-23 09:30:41 +0100

    react project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LoginPage from "./components/LoginPage/LoginPage";
+import Chart from "./components/Chart/Chart";
+import Login from "./components/LoginPage/LoginPage";
+
+function App() {
+  return (
+    <div>
+        {/*<Chart/>*/}
+        {/*<Login/>*/}
+        <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
